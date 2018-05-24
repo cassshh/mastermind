60899f8f4d1127cf8c5edb8151fb2e7b6853a67a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-24 23:59:10 +0000

    Deploy cassshh/Mastermind to github.com/cassshh/Mastermind.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,11 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(
-      new Response({
-        headers: new Headers({ 'Content-Type': 'text/html' }),
-        status: 200,
-        body: '<h1>U offline bro</h1>'
-      })
+      () =>
+        new Response('<h1>U offline bro</h1>', {
+          headers: new Headers({ 'Content-Type': 'text/html' }),
+          status: 200
+        })
     )
   );
 });
